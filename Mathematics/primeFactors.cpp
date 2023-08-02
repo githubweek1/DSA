c81161ec864195833c10a9547be254d26cd91f12
smdekate <90138014+smdekate@users.noreply.github.com>
2023-08-02 08:05:45 +0000

    efficient approach

--- Mathematics/primeFactors.cpp
+++ Mathematics/primeFactors.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 // function prototype
 void primeFactors(int n);
-bool isPrime(int n);
 
 int main() {
     int n;
@@ -16,29 +15,16 @@ int main() {
 }
 
 void primeFactors(int n) {
-    for (int i = 2; i < n; i++) {
-        if (isPrime(i)) {
-            int x = i;
-            while (n % x == 0) {
-                cout << i << " ";
-                x = x * i;
-            }
+    if (n <= 1) return;
+
+    for (int i = 2; i*i <= n; i++) {
+        while (n % i == 0) {
+            cout << i << " ";
+            n = n / i;
         }
     }
+    if (n > 1)
+        cout << n;
 
     cout << endl;
-}
-
-bool isPrime(int n) {
-    if (n == 1) return false;
-    if (n == 2 || n == 3) return true;
-
-    if (n % 2 == 0 || n % 3 == 0)
-        return false;
-    
-    for (int i = 5; i*i <= n; i += 6) {
-        if (n % i == 0 || n % (i + 2) == 0)
-            return false;
-    }
-    return true;
 }
