431e3246cd543cc12b16cafe55ceceef4e50e3a8
smdekate <90138014+smdekate@users.noreply.github.com>
2023-08-02 07:17:08 +0000

    Even more efficient approach

--- Mathematics/isPrime.cpp
+++ Mathematics/isPrime.cpp
@@ -19,12 +19,14 @@ int main() {
 
 // function to check if the number is prime
 bool isPrime(int n) {
+    // corner cases
+    if (n == 1) return false;
+    if (n == 2 || n == 3) return true;
     
-    if (n == 1)
+    if (n % 2 == 0 || n % 3 == 0)
         return false;
-
-    for (int i = 2; i*i <= n; i++) {
-        if (n % i == 0) 
+    for (int i = 5; i*i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0)
             return false;
     }
     return true;
