b31d2131fe9d6289338d13f6f1434047637620fb
smdekate <90138014+smdekate@users.noreply.github.com>
2023-08-02 09:41:58 +0000

    optimization done

--- Mathematics/seive.cpp
+++ Mathematics/seive.cpp
@@ -22,7 +22,7 @@ void sieve(int n) {
 
     for (int i = 2; i*i <= n; i++) {
         if (isPrime[i]) {
-            for (int j = 2*i; j <= n; j = j + i) {
+            for (int j = i*i; j <= n; j = j + i) {
                 isPrime[j] = false;
             }
         }
